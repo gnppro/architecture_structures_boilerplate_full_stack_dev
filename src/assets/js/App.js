2a3b0205a0ae10cac0e32546b0c089d17bf32b57
Gerardo Nava Pereda <gerardo.nava.pereda@gmail.com>
2018-07-17 00:55:12 -0500

    Se agrego component App y Hola Mundo de Reactjs

---
+++ src/assets/js/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+// import './App.css';
+
+class App extends Component {
+  render() {
+    return ( 
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Bienvenido a React</h1>
+        </header>
+        <p className="App-intro">
+          Para empezar, edita el componente App<code>src/assets/App.js</code> guarda y recarga.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,7 +1,17 @@
-import '../sass/main.scss';
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import './index.css';
+import App from './App';
 import { firstMessage } from './message.js';
+import '../sass/main.scss';
+
+const container = document.getElementById('main');
+const holaMundo = <h1>Hola Mundo</h1>;
+
+ReactDOM.render(holaMundo, container);
+ReactDOM.render(<App />, container);
 
 document.write("HOLA DESDE WEBPACK + SASS + BABEL + Images + Fonts")
-document.write("<br/> SERVER CON AUTO-RECARGADO YA FUNCIONA!")
+document.write("<br/> SERVER CON AUTO-RECARGADO YA FUNCIONA!, ReactDOM no sirve")
 
 document.write("<br/>", firstMessage)
