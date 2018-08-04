0ca72e4976d24b01d7569f74384af5e2cc089d30
Gerardo Nava Pereda <gerardo.nava.pereda@gmail.com>
2018-08-04 03:03:07 -0500

    Agregue import de createStore Redux, reducer y redux-dev-tools-extension

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import reducer from './reducer';
+
 // import './index.css';
 import App from './App';
 import { firstMessage } from './message.js';
 import '../sass/main.scss';
+
+const store = createStore(
+    reducer, /* preloadedState, */
+ +  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
 
 const container = document.getElementById('main');
 const holaMundo = <h1>Hola Mundo</h1>;
@@ -12,6 +20,6 @@ ReactDOM.render(holaMundo, container);
 ReactDOM.render(<App />, container);
 
 document.write("HOLA DESDE WEBPACK + SASS + BABEL + Images + Fonts")
-document.write("<br/> SERVER CON AUTO-RECARGADO YA FUNCIONA!, ReactDOM no sirve")
+document.write("<br/> SERVER CON AUTO-RECARGADO YA FUNCIONA!, ReactDOM Listo!")
 
 document.write("<br/>", firstMessage)
---
+++ src/assets/js/reducer.js
@@ -0,0 +1,50 @@
+import axios from 'axios'
+
+const ADD_TOKEN = 'ADD_TOKEN';
+const ITEMS = 'ITEMS';
+
+export const addToken = (token) => ({ type: ADD_TOKEN, payload: token });
+export const addDataItems = (data) => ({ type: ITEMS, payload: data });
+
+// export function asyncCallItems(dispatch) {
+//     return dispatch 
+//     axios.get('https://bedu.safemeet.space/items')
+//         .then(({ data })=> {
+//             console.log(data);
+//           })
+//           .catch(function (error) {
+//             console.log(error);
+//           });
+//   }
+function asyncCallItems() {
+    return dispatch => {
+        axios.get('https://bedu.safemeet.space/items')
+        .then((res) => {
+            let data = res.data
+            dispatch(addDataItems(data));
+            console.log(addDataItems);
+        })
+        .catch(function (error) {
+        console.log(error);
+        });   
+    };
+  }
+
+const intialState = {
+    token: localStorage.getItem('token'),
+    data: localStorage.getItem('data'),
+}
+
+export default (state = intialState, action) => {
+    console.log('state', state);
+    console.log('action', action);
+    switch (action.type) {
+        case ADD_TOKEN:
+            localStorage.setItem('token', action.payload);
+            return {...state, token: action.payload};
+        case ITEMS:
+            return {...state, data: action.payload };
+        default:
+            return state
+    }
+}
